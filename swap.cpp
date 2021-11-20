efc7b99f4dcc026e86964b0fee643e0c172e17fc
Prashant Birajdar <92636189+Superprashant@users.noreply.github.com>
2021-11-20 19:32:57 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{string a="25",b="39";
+cout<<"Input 1st number : " <<a<<endl;
+cout<<"Input 2nd number : " <<b<<endl;
+a[0]='3';
+a[1]='9';
+cout<<"After swapping 1st number:"<<a<<endl;
+b[0]='2';
+b[1]='5';
+cout<<"After swapping 2nd number:"<<b;
+return 0;
+}
